616e0b263bf801bc37721e6b70fe92aaf27f982a
Ruii1112 <ruiiiii1112@gmail.com>
2022-06-16 22:10:39 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2022_06_16_220430_create_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->integer('use_id')->unsigned();
+            $table->integer('shift_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
