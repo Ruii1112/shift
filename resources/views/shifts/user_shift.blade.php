1d81fe81fa2e735ec71afb25fc9a8aaaff6005da
Ruii1112 <ruiiiii1112@gmail.com>
2022-07-30 20:28:43 +0900

    [add]スプレッドシート反映機能

--- resources/views/shifts/user_shift.blade.php
+++ resources/views/shifts/user_shift.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
         <h2>シフト希望</h2>
-        <form action="/user/shift/submit" method="POST">
-        @csrf
-            <reflection-component :times="{{ json_encode($times)}}" :youbi_shift="{{ json_encode($youbi) }}"></reflection-component>
-            <h3>コメント</h3>
-                <textarea name="time[comment]" cols="80" rows="10"></textarea>
-            </div>
-            <p></p>
-            <input type="submit" value="提出"/>
-        </form>
-        <div><p><a href="/user/index">戻る<a></div>
+        <div class="form">
+            <form action="/user/shift/submit" method="POST">
+            @csrf
+                <reflection-component :times="{{ json_encode($times)}}" :youbi_shift="{{ json_encode($youbi) }}"></reflection-component>
+                <h3>コメント</h3>
+                    <textarea name="time[comment]" cols="80" rows="10"></textarea>
+                <p></p>
+                <input type="hidden" name="time[shift_id]" value="{{$shift}}"/>
+                <input type="submit" value="提出"/>
+            </form>
+        </div>
+        <div><p><a href="/user/index">戻る</a></div>
         
-        <a href="/sheet">スプレッドシート</a>
 @endsection
